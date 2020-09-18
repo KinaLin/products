4df208c02304d12cb145ef6b737c7fa6003f067f
KinaLin <tea30826@gmail.com>
2020-09-18 14:26:58 +0800

    fifth

--- products.py
+++ products.py
@@ -1,12 +1,17 @@
+import os
 #讀取檔案
 products = []
-with open ('products.csv', 'r', encoding='utf-8') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue
-		name, price = line.strip().split(',')
-		products.append([name, price])
+if os.path.isfile('products.csv'):
+	print('有')
+	with open ('products.csv', 'r', encoding='utf-8') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue
+			name, price = line.strip().split(',')
+			products.append([name, price])
 	print(products)
+else:
+	print('無')
 #讓使用者輸入
 while True:
 	name = input('請輸入商品名稱:')
